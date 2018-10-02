d89e31e818329731535908f53bf0cf669f2cc2a9
ClaireBvs <clairembeauvais@gmail.com>
2018-10-01 22:29:16 -0600

    add routes for merchants find/params id and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
+
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
     end
   end
 end
