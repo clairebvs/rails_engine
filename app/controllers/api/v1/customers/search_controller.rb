4503ce62110987323255c8ffe303b7e48b85f1e3
ClaireBvs <clairembeauvais@gmail.com>
2018-10-02 16:04:44 -0600

    add serializer for customer and refactor search controller

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -9,9 +9,11 @@ class Api::V1::Customers::SearchController < ApplicationController
   end
 
   def show
-    render json: Customer.find_by('LOWER(first_name) = ?', params[:first_name].downcase) if params[:first_name]
-    render json: Customer.find_by('LOWER(last_name) = ?', params[:last_name].downcase) if params[:last_name]
-    if params[:id] || params[:created_at] || params[:updated_at]
+    if params[:first_name]
+      render json: Customer.find_by('LOWER(first_name) = ?', params[:first_name].downcase)
+    elsif params[:last_name]
+      render json: Customer.find_by('LOWER(last_name) = ?', params[:last_name].downcase)
+    elsif params[:id] || params[:created_at] || params[:updated_at]
       render json: Customer.find_by(customer_params)
     end
   end
