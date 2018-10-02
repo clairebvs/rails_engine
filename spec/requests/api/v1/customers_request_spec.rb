1b952742045780b18dde70b63ea1fc857b73fe97
ClaireBvs <clairembeauvais@gmail.com>
2018-10-02 11:49:54 -0600

    add customer request test find by params id

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -12,7 +12,7 @@ describe 'Customers API' do
     expect(customers.count).to eq 4
   end
 
-  it 'can get one custoemr by its id' do
+  it 'can get one customer by its id' do
     id = create(:customer).id
 
     get "/api/v1/customers/#{id}"
@@ -21,5 +21,19 @@ describe 'Customers API' do
 
     expect(response).to be_successful
     expect(customer["id"]).to eq(id)
+  end
+
+  # FINDER find
+
+  it 'can find one customer by params id' do
+    customer = create(:customer, id: 1)
+    customer_2 = create(:customer, id: 2)
+
+    get '/api/v1/customers/find?id=2'
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["id"]).to eq(customer_2.id)
   end
 end
