b37a3ab96d46460883d1d354abef216c0cb37b86
ClaireBvs <clairembeauvais@gmail.com>
2018-10-02 11:42:00 -0600

    add customers request show test

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -11,4 +11,15 @@ describe 'Customers API' do
     expect(response).to be_successful
     expect(customers.count).to eq 4
   end
+
+  it 'can get one custoemr by its id' do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["id"]).to eq(id)
+  end
 end
