85d33e1ce218625830a316018aa2be1fa8a5a8aa
ClaireBvs <clairembeauvais@gmail.com>
2018-10-03 14:58:39 -0600

    add relationships and validation to model Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :unit_price
 
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  belongs_to :merchant
 end
