a66f8a629edefc2d54e823f114f5d7abf8fbcb42
ClaireBvs <clairembeauvais@gmail.com>
2018-10-03 13:46:20 -0600

    add validatuons and relationships test for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe 'validations' do
+    it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
+  end
+  describe 'relationships' do
+    it {should have_many(:invoices)}
+    it {should have_many(:merchants).through(:invoices)}
+  end
+end 
