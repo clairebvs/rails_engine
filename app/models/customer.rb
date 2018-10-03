92354ae0682c85ef5a7d506cd6c26d4b023fdce0
ClaireBvs <clairembeauvais@gmail.com>
2018-10-03 15:16:34 -0600

    add relationships to Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,6 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
 
   has_many :invoices
-  has_many :invoices, through: :merchants
+  has_many :merchants, through: :invoices
+  has_many :transactions, through: :invoices
 end
