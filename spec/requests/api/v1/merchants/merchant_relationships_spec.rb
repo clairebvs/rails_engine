4f412cd03249e1543572f224002d9a79d6a3a53a
ClaireBvs <clairembeauvais@gmail.com>
2018-10-03 21:07:19 -0600

    add request test merchant relationship for get /api/v1/merchants/#{id}/invoices

--- spec/requests/api/v1/merchants/merchant_relationships_spec.rb
+++ spec/requests/api/v1/merchants/merchant_relationships_spec.rb
@@ -12,8 +12,23 @@ describe 'Merchants relationships endpoint' do
 
       items = JSON.parse(response.body)
 
-      expect(response).to be_successful
       expect(items.count).to eq(5)
+    end
+  end
+
+  context 'GET /api/v1/merchants/:id/invoices' do
+    it 'returns a collection of invoices associated with that merchant from their known orders' do
+      id = create(:merchant).id
+      customer_id = create(:customer).id
+      create_list(:invoice, 4, merchant_id: id, customer_id: customer_id)
+
+      get "/api/v1/merchants/#{id}/invoices"
+
+      expect(response).to be_successful
+
+      invoices = JSON.parse(response.body)
+
+      expect(invoices.count).to eq(5)
     end
   end
 end
