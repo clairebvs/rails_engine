d536e269b77075c3c242ed33d12f84b9faaf38de
ClaireBvs <clairembeauvais@gmail.com>
2018-10-04 21:44:33 -0600

    add request test find by params id for invoices

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
         get '/:id/transactions', to: 'transactions#index'
       end
 
+      namespace :invoices do
+        get '/find', to: 'search#show'
+      end
+
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
