f4dda911732ea4c927c20c2af8388bab2be66049
ClaireBvs <clairembeauvais@gmail.com>
2018-10-05 11:21:40 -0600

    add routes, controller and action show for Invoice/MerchantController

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
         get '/:id/invoice_items', to: 'invoice_items#index'
         get '/:id/items', to: 'items#index'
         get '/:id/customer', to: 'customer#show'
+        get '/:id/merchant', to: 'merchant#show'
       end
 
       namespace :transactions do
