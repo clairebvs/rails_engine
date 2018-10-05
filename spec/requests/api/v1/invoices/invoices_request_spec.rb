bd50d48b0db30f7a12e16a5cc9d6ce299c607eb0
ClaireBvs <clairembeauvais@gmail.com>
2018-10-04 21:37:44 -0600

    add invoices request test for show

--- spec/requests/api/v1/invoices/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices/invoices_request_spec.rb
@@ -13,4 +13,17 @@ describe 'Invoices API' do
     expect(response).to be_successful
     expect(invoices.count).to eq 9
   end
+
+  it 'can get one invoice by its id' do
+    merchant_id = create(:merchant).id
+    customer_id = create(:customer).id
+    id = create(:invoice, merchant_id: merchant_id, customer_id: customer_id).id
+
+    get "/api/v1/invoices/#{id}"
+
+    invoice = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice["id"]).to eq(id)
+  end
 end
