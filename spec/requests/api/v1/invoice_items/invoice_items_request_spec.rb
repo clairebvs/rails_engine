1e56f8b15da6dbff8c7a2df036f0efad4a8365a3
ClaireBvs <clairembeauvais@gmail.com>
2018-10-06 13:37:49 -0600

    add request test for invoice_items index

---
+++ spec/requests/api/v1/invoice_items/invoice_items_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Invoice Items API' do
+  it 'sends a list of invoice_items' do
+    merchant_id = create(:merchant).id
+    customer_id = create(:customer).id
+    invoice_id = create(:invoice, merchant_id: merchant_id, customer_id: customer_id).id
+    item_id = create(:item, merchant_id: merchant_id).id
+    create_list(:invoice_item, 5, item_id: item_id, invoice_id: invoice_id)
+
+    get '/api/v1/invoice_items'
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice_items.count).to eq 5
+  end
+end
