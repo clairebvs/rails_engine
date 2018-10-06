c324e61b70addd382d91ae6c6a2c7e1b044004b8
ClaireBvs <clairembeauvais@gmail.com>
2018-10-05 20:07:30 -0600

    add routes, controller and action show to find random invoice

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
       namespace :invoices do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
+        get '/random', to: 'random#show'
         get '/:id/transactions', to: 'transactions#index'
         get '/:id/invoice_items', to: 'invoice_items#index'
         get '/:id/items', to: 'items#index'
