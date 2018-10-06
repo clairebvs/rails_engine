724508f329d59ad99a98c8de935b47994040954d
ClaireBvs <clairembeauvais@gmail.com>
2018-10-06 09:16:17 -0600

    add request test find item by params id

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -24,4 +24,18 @@ describe 'Items API' do
     expect(response).to be_successful
     expect(item["id"]).to eq(id)
   end
+
+  # FINDER find?query=parameter
+  it 'can find one item by params id' do
+    merchant_id = create(:merchant).id
+    item_1 = create(:item, id: 1, merchant_id: merchant_id)
+    item_2 = create(:item, id: 2, merchant_id: merchant_id)
+
+    get '/api/v1/items/find?id=2'
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item["id"]).to eq(item_2.id)
+  end
 end
