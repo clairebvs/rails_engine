c5b9d111ba64f2c78b74627191eb9156aa128e76
ClaireBvs <clairembeauvais@gmail.com>
2018-10-05 20:22:22 -0600

    add routes and action show for Transactions controller

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
-      resources :transactions, only: [:index]
+      resources :transactions, only: [:index, :show]
     end
   end
 end
