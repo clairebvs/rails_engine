bc74c99376d67a4f31136cba7ca95ee42802fd2a
ClaireBvs <clairembeauvais@gmail.com>
2018-10-05 20:50:08 -0600

    add routes and index action for Transactions::SearchController

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
 
       namespace :transactions do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
         get '/:id/invoice', to: 'invoice#show'
       end
 
