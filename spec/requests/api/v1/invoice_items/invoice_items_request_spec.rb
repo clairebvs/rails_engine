542d545ffd616e9fed7fbb64c04e58f129993c1b
ClaireBvs <clairembeauvais@gmail.com>
2018-10-06 13:43:43 -0600

    implements routes, controller and action index and show for invoice_items

--- spec/requests/api/v1/invoice_items/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_items_request_spec.rb
@@ -15,4 +15,19 @@ describe 'Invoice Items API' do
     expect(response).to be_successful
     expect(invoice_items.count).to eq 5
   end
+
+  it 'can get one invoice_items' do
+    merchant_id = create(:merchant).id
+    customer_id = create(:customer).id
+    invoice_id = create(:invoice, merchant_id: merchant_id, customer_id: customer_id).id
+    item_id = create(:item, merchant_id: merchant_id).id
+    id = create(:invoice_item, item_id: item_id, invoice_id: invoice_id).id
+
+    get "/api/v1/invoice_items/#{id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice_item["id"]).to eq(id)
+  end
 end
