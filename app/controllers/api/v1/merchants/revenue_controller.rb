dcae6b083b78fb36c33a9e868c948c3c95da607e
ClaireBvs <clairembeauvais@gmail.com>
2018-10-07 11:27:49 -0600

    add to revenueController parsing date

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
     if params[:date]
-      merchant_date_revenue = Merchant.total_revenue_for_a_merchant_on_date(params[:date], params[:id]).round(2)/100
+      date = Date.parse("#{params[:date]} 00:00:00 UTC")
+      merchant_date_revenue = Merchant.total_revenue_for_a_merchant_on_date(date, params[:id]).round(2)/100
       render json: {"revenue" => "#{merchant_date_revenue}"}
     else
       merchant_revenue = Merchant.total_revenue_for_a_merchant(params[:id]).round(2) /100
