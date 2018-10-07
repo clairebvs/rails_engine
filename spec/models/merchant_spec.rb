b81d5655c190d92485f5898c0cbe9db58e5f302c
ClaireBvs <clairembeauvais@gmail.com>
2018-10-07 11:02:24 -0600

    add model test for Merchant.total_revenue_for_a_merchant_on_date

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -98,4 +98,28 @@ RSpec.describe Merchant, type: :model do
     end
   end
 
+  describe '.total_revenue_for_a_merchant_for_date' do
+    it 'should return the total revenue for one merchant accross successful transactions on a date' do
+      date = '2018-03-04'
+      merchant = create(:merchant, name: 'Joe', id: 2)
+
+      customer_1 = create(:customer, id: 1)
+      customer_2 = create(:customer, id: 2)
+
+      invoice_id_1 = create(:invoice, merchant_id: merchant.id, customer_id: customer_1.id, updated_at: '2017-12-12').id
+      invoice_id_2 = create(:invoice, merchant_id: merchant.id, customer_id: customer_1.id, updated_at: date).id
+
+      item_id = create(:item, merchant_id: merchant.id).id
+      item_id_2 = create(:item, merchant_id: merchant.id).id
+
+      invoice_items_1 = create(:invoice_item, invoice_id: invoice_id_1, item_id: item_id, quantity: 2, unit_price: 4)
+      invoice_items_2 = create(:invoice_item, invoice_id: invoice_id_2, item_id: item_id_2, quantity: 2, unit_price: 5)
+
+      transaction = create(:transaction, invoice_id: invoice_id_1, result: 'success')
+      transaction2 = create(:transaction, invoice_id: invoice_id_2, result: 'success')
+
+      expect(Merchant.total_revenue_for_a_merchant_on_date(date, merchant.id)).to eq(10)
+    end
+  end
+
 end
